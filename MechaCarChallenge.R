1b20930ad12d74e629e20f2813fce6c1fdd97de3
jozhen <89761792+jozhen@users.noreply.github.com>
2021-12-07 19:54:53 -0800

    Update MechaCarChallenge.R

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -8,3 +8,11 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 suspen_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F) 
 total_summary <- suspen_table%>%summarize(Mean=mean(PSI),Median=median(PSI),Var=var(PSI),SD=sd(PSI),.groups = 'keep')
 lot_summary <- suspen_table%>%group_by(Manufacturing_Lot)%>%summarize(Mean=mean(PSI),Median=median(PSI),Var=var(PSI),SD=sd(PSI),.groups = 'keep')
+##Deliverable 3
+t.test(suspen_table$PSI,mu=1500)
+lot1 <- subset(suspen_table, Manufacturing_Lot=="Lot1")
+lot2 <- subset(suspen_table, Manufacturing_Lot=="Lot2")
+lot3 <- subset(suspen_table, Manufacturing_Lot=="Lot3")
+t.test(lot1$PSI,mu=1500)
+t.test(lot2$PSI,mu=1500)
+t.test(lot3$PSI,mu=1500)
